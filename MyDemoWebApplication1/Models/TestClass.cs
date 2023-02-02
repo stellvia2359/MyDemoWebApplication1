5711aa74945df0f64758e0ac4b1bc37048ce6d47
Jimmy Huang <stellvia2359@gmail.com>
2023-02-02 11:04:56 +0800

    piggy

---
+++ MyDemoWebApplication1/Models/TestClass.cs
@@ -0,0 +1,16 @@
+﻿namespace MyDemoWebApplication1.Models
+{
+    public static class TestStaticClass
+    {
+        private static IConfiguration Configuration;
+
+        public static void Initialize(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public static void Method1()
+        {
+            var t = Configuration.GetSection("Test").GetChildren();
+        }
+    }
+}
